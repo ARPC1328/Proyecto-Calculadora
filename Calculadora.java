6be7a8d31c2182b4b6f9cb7a84576abfd2d338b6
Brayan-exe <78459098+Brayan-exe@users.noreply.github.com>
2021-02-18 20:42:20 -0600

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -5,14 +5,18 @@
  */
 package calculadora;
 
+import java.text.DecimalFormat;
+
 /**
  *
- * @author Usuario
+ * @author Brayansito
  */
 public class Calculadora {
+
     private String expresion;
 
     public Calculadora() {
+
     }
 
     public Calculadora(String expresion) {
@@ -25,7 +29,114 @@ public class Calculadora {
 
     public void setExpresion(String expresion) {
         this.expresion = expresion;
-    } 
+    }
+
+    public int jerarquiaOperaciones(String simbolo) {
+        int res = -2;
+
+        if (simbolo.equals("+") || simbolo.equals("-")) {
+            res = -1;
+        } else {
+            if (simbolo.equals("*") || simbolo.equals("/")) {
+                res = 0;
+            } 
+        }
+
+        return res;
+    }
+
+    public boolean balanceoParentesis() {
+        boolean res = true;
+        int i;
+        PilaArre<Character> p = new PilaArre();
+        char simbolo;
+
+        i = 0;
+        while (i < expresion.length() && res) {
+            simbolo = expresion.charAt(i);
+            switch (simbolo) {
+                case '(':
+                    p.push(simbolo);
+                    break;
+                case ')':
+                    if (p.isEmpty()) {
+                        res = false;
+                    } else {
+                        p.pop();
+                    }
+                    break;
+            }
+            i++;
+        }
+
+        return res && p.isEmpty();
+    }
+
+    public PilaADT<String> expresionCalculadora() {
+        PilaArre cad = new PilaArre();
+        String simbolo;
+        PilaADT<String> cal = new PilaArre();
+        String[] arre;
+
+        if (balanceoParentesis()) {
+            arre = expresion.split(" ");
+            for (int i = 0; i < arre.length; i++) {
+                simbolo = arre[i];
+                switch (simbolo) {
+                    case "(": //Parentesis izquierdo
+                        cal.push(simbolo);
+                        break;
+                    case "+": //operadores
+                    case "-":
+                    case "*":
+                    case "/":
+                    case "^":
+                        while (!cal.isEmpty() && (jerarquiaOperaciones(simbolo) <= jerarquiaOperaciones(cal.peek()))) {
+                            cad.push(cal.pop());
+                        }
+                        cal.push(simbolo);
+                    break;
+                    case ")": //Parentesis derecho
+                        while (!cal.isEmpty() && !cal.peek().equals("(")) {
+                            cad.push(cal.pop());
+                        }
+                        cal.pop();
+                    break;   
+                    default: //operando
+                        cad.push(simbolo);
+                    break;
+                }
+            }
+            while (!cal.isEmpty()){
+                arre = expresion.split(" ");
+                cad.push(cal.pop());
+            }
+        } else {
+            cad.push("E R R O R S I N T A X I S");
+        }
+
+        return cad;
+    }
+
+    public static <T> String imprime(PilaADT<T> PilaArre) { //siempre ADT para irme a lo genereal y que pueda incluir todo 
+        StringBuilder sb = new StringBuilder();
+        PilaArre<T> aux = new PilaArre();
+        
+        while (!PilaArre.isEmpty()) {
+            sb.append(PilaArre.peek().toString() + "\n");
+            aux.push(PilaArre.pop());
+
+        }
+        while (!aux.isEmpty()) {
+            PilaArre.push(aux.pop());
+        }
+        return sb.toString();
+
+    }
+
+    public boolean esSigno(String X) {
+        return (X.equals("+") || X.equals("-") || X.equals("*") || X.equals("/") || X.equals("^"));
+    }
     
     public boolean revisionSintaxis() {
         boolean res = true;
@@ -38,75 +149,77 @@ public class Calculadora {
         i = 0;
         while (i < expresion.length() && res) {
             simbolo = expresion.charAt(i);
-            switch (simbolo) {
-                case '(':
-                    if(!signos.isEmpty())     
-                        if(signos.peek().equals(')'))
+            if (simbolo != ' ') {
+                switch (simbolo) {
+                    case '(':
+                        if(!signos.isEmpty())     
+                            if(signos.peek().equals(')'))
+                                res = false;
+                        parentesis.push(simbolo);                    
+                        signos.push(simbolo);                    
+                        break;
+                    case ')':
+                        if(parentesis.isEmpty())
                             res = false;
-                    parentesis.push(simbolo);                    
-                    signos.push(simbolo);                    
-                    break;
-                case ')':
-                    if(parentesis.isEmpty())
-                        res = false;
-                    else 
-                        parentesis.pop();                        
-                    if(!signos.isEmpty()) 
-                        if(signos.peek().equals('('))
+                        else 
+                            parentesis.pop();                        
+                        if(!signos.isEmpty()) 
+                            if(signos.peek().equals('('))
+                                res = false;
+                        signos.push(simbolo);
+                        break;
+                    case '+':
+                        if(!operadores.isEmpty())
+                            if(operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
+                                res = false;
+                        else {
+                            if(!puntos.isEmpty()) 
+                                puntos.pop();                     
+                            operadores.push(simbolo);
+                            signos.push(simbolo);
+                        }
+                        break;
+                    case '-':
+                        if(!operadores.isEmpty())
+                            if(operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
+                                res = false;
+                        else {
+                            if(!puntos.isEmpty())
+                                puntos.pop();                      
+                            operadores.push(simbolo);
+                            signos.push(simbolo);
+                        }                  
+                        break;
+                    case '*':
+                        if(operadores.isEmpty() || operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
                             res = false;
-                    signos.push(simbolo);
-                    break;
-                case '+':
-                    if(!operadores.isEmpty())
-                        if(operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
+                        else {
+                            if(!puntos.isEmpty())
+                                puntos.pop();                       
+                            operadores.push(simbolo); 
+                            signos.push(simbolo);
+                        }                  
+                        break;
+                    case '/':
+                        if(operadores.isEmpty() || operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
                             res = false;
-                    else {
-                        if(!puntos.isEmpty()) 
-                            puntos.pop();                     
+                        else {
+                            if(!puntos.isEmpty())
+                                puntos.pop();
+                            operadores.push(simbolo);
+                            signos.push(simbolo);
+                        }                
+                        break;
+                    case '.':
+                        if(puntos.isEmpty())
+                            puntos.push(simbolo);
+                        else
+                            res = false;
                         operadores.push(simbolo);
-                        signos.push(simbolo);
-                    }
-                    break;
-                case '-':
-                    if(!operadores.isEmpty())
-                        if(operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
-                            res = false;
-                    else {
-                        if(!puntos.isEmpty())
-                            puntos.pop();                      
+                        break;
+                    default: 
                         operadores.push(simbolo);
-                        signos.push(simbolo);
-                    }                  
-                    break;
-                case '*':
-                    if(operadores.isEmpty() || operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
-                        res = false;
-                    else {
-                        if(!puntos.isEmpty())
-                            puntos.pop();                       
-                        operadores.push(simbolo); 
-                        signos.push(simbolo);
-                    }                  
-                    break;
-                case '/':
-                    if(operadores.isEmpty() || operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
-                        res = false;
-                    else {
-                        if(!puntos.isEmpty())
-                            puntos.pop();
-                        operadores.push(simbolo);
-                        signos.push(simbolo);
-                    }                
-                    break;
-                case '.':
-                    if(puntos.isEmpty())
-                        puntos.push(simbolo);
-                    else
-                        res = false;
-                    operadores.push(simbolo);
-                    break;
-                default: 
-                    operadores.push(simbolo);
+                }
             }
             i++;
         }
@@ -118,8 +231,57 @@ public class Calculadora {
         return res;
     }  
     
+    public double resuelveExpresion(PilaADT<String> pila){
+        double res = 0;
+        PilaADT <String> aux = new PilaArre();
+        PilaADT <String> aux1 = new PilaArre();
+        PilaADT <String> aux2 = new PilaArre();
+        String simbolo;
+        
+        while (!pila.isEmpty()){
+            aux.push(pila.pop());
+        }
+        
+        while (!aux.isEmpty()){
+            simbolo = aux.pop();
+            switch (simbolo){
+                case "+":
+                    res = Double.parseDouble(aux1.pop()) + Double.parseDouble(aux1.pop());
+                    aux1.push(String.valueOf(res));
+                break;    
+                case "-":
+                    aux2.push(aux1.pop());
+                    res = Double.parseDouble(aux1.pop()) - Double.parseDouble(aux2.pop());
+                    aux1.push(String.valueOf(res));
+                break;
+                case "*":    
+                    res = Double.parseDouble(aux1.pop()) * Double.parseDouble(aux1.pop());
+                    aux1.push(String.valueOf(res));
+                break;
+                case "/":
+                    aux2.push(aux1.pop());
+                    res = Double.parseDouble(aux1.pop()) / Double.parseDouble(aux2.pop());  
+                    aux1.push(String.valueOf(res));
+                break;
+                default:
+                    aux1.push(simbolo);
+                break;    
+            }
+        }
+        
+        return Double.parseDouble (aux1.peek());
+    }            
+   
     public static void main(String[] args) {
-        Calculadora cal = new Calculadora("+((5/2+3)+((5/2)))");
-        System.out.println(cal.revisionSintaxis());
-    }   
-}
+        Calculadora cal = new Calculadora("9 / -1");
+        PilaADT<String> pila = new PilaArre();
+        //Calculadora cal2 = new Calculadora ();
+        
+        pila = cal.expresionCalculadora();
+        System.out.println (imprime (pila));
+        System.out.println (cal.resuelveExpresion(pila));
+        
+
+    }
+}   
+
