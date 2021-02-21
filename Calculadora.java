2513cc5e6ab9447ea88a8447749d5629ff5ca687
ARPC1328 <78459442+ARPC1328@users.noreply.github.com>
2021-02-21 01:31:28 -0600

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -72,124 +72,126 @@ public class Calculadora {
         i = 0;
         while (i < expresion.length() && res) {
             simbolo = expresion.charAt(i);
-            switch (simbolo) {
-                case '(':
-                    if(!signos.isEmpty())     
-                        if(signos.peek().equals(')') && parentesis2.isEmpty())
+            if(simbolo != ' ') {
+                switch (simbolo) {
+                    case '(':
+                        if(!signos.isEmpty())     
+                            if(signos.peek().equals(')') && parentesis2.isEmpty())
+                                res = false;
+                        if(!parentesis2.isEmpty())
+                            while(!parentesis2.isEmpty())
+                                parentesis2.pop();
+                        parentesis.push(simbolo);                    
+                        signos.push(simbolo);                    
+                        break;
+                    case ')':
+                        if(parentesis.isEmpty())
                             res = false;
-                    if(!parentesis2.isEmpty())
-                        while(!parentesis2.isEmpty())
-                            parentesis2.pop();
-                    parentesis.push(simbolo);                    
-                    signos.push(simbolo);                    
-                    break;
-                case ')':
-                    if(parentesis.isEmpty())
-                        res = false;
-                    else 
-                        parentesis.pop();                        
-                    if(!signos.isEmpty()) 
-                        if(signos.peek().equals('(') && parentesis2.isEmpty())
+                        else 
+                            parentesis.pop();                        
+                        if(!signos.isEmpty()) 
+                            if(signos.peek().equals('(') && parentesis2.isEmpty())
+                                res = false;
+                        if(!parentesis2.isEmpty())
+                            while(!parentesis2.isEmpty())
+                                parentesis2.pop();
+                        if(!division.isEmpty()) {
+                            cero = contarPila(casoCero);
+                            total = 0;
+                            while(!casoCero.isEmpty()) 
+                                if(casoCero.pop().equals('0'))
+                                    total++;
+                            if(cero == total)
+                                res = false;
+                            division.pop();                        
+                        }
+                        signos.push(simbolo);
+                        break;
+                    case '+':
+                        if(!operadores.isEmpty())
+                            if(operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
+                                res = false;
+                        else {
+                            if(!puntos.isEmpty()) 
+                                puntos.pop();                     
+                            operadores.push(simbolo);
+                            if(!signos.isEmpty())
+                                if(signos.peek().equals('/'))
+                                    if(!division.isEmpty()) {
+                                        cero = contarPila(casoCero);
+                                        total = 0;
+                                        while(!casoCero.isEmpty()) 
+                                            if(casoCero.pop().equals('0'))
+                                                total++;
+                                        if(cero == total)
+                                            res = false;
+                                        division.pop();
+                            }  
+                            signos.push(simbolo);
+                        }
+                        break;
+                    case '-':
+                        if(!puntos.isEmpty())
+                                puntos.pop();                      
+                        operadores.push(simbolo);
+                        signos.push(simbolo);                 
+                        break;
+                    case '*':
+                        if(operadores.isEmpty() || operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
                             res = false;
-                    if(!parentesis2.isEmpty())
-                        while(!parentesis2.isEmpty())
-                            parentesis2.pop();
-                    if(!division.isEmpty()) {
-                        cero = contarPila(casoCero);
-                        total = 0;
-                        while(!casoCero.isEmpty()) 
-                            if(casoCero.pop().equals('0'))
-                                total++;
-                        if(cero == total)
+                        else {
+                            if(!puntos.isEmpty())
+                                puntos.pop();                       
+                            operadores.push(simbolo); 
+                            if(!division.isEmpty()) {
+                                cero = contarPila(casoCero);
+                                total = 0;
+                                while(!casoCero.isEmpty()) 
+                                    if(casoCero.pop().equals('0'))
+                                        total++;
+                                if(cero == total)
+                                    res = false;
+                                division.pop();
+                            }  
+                            signos.push(simbolo);
+                        }                  
+                        break;
+                    case '/':
+                        if(operadores.isEmpty() || operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
                             res = false;
-                        division.pop();                        
-                    }
-                    signos.push(simbolo);
-                    break;
-                case '+':
-                    if(!operadores.isEmpty())
-                        if(operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
+                        else {
+                            if(!puntos.isEmpty())
+                                puntos.pop();
+                            operadores.push(simbolo);
+                            if(!division.isEmpty()) {
+                                cero = contarPila(casoCero);
+                                total = 0;
+                                while(!casoCero.isEmpty()) 
+                                    if(casoCero.pop().equals('0'))
+                                        total++;
+                                if(cero == total)
+                                    res = false;
+                                division.pop();
+                            }     
+                            signos.push(simbolo);                        
+                            division.push(simbolo);
+                        }                
+                        break;
+                    case '.':
+                        if(puntos.isEmpty())
+                            puntos.push(simbolo);
+                        else
                             res = false;
-                    else {
-                        if(!puntos.isEmpty()) 
-                            puntos.pop();                     
+                        operadores.push(simbolo);
+                        break;
+                    default: 
                         operadores.push(simbolo);
                         if(!signos.isEmpty())
-                            if(signos.peek().equals('/'))
-                                if(!division.isEmpty()) {
-                                    cero = contarPila(casoCero);
-                                    total = 0;
-                                    while(!casoCero.isEmpty()) 
-                                        if(casoCero.pop().equals('0'))
-                                            total++;
-                                    if(cero == total)
-                                        res = false;
-                                    division.pop();
-                        }  
-                        signos.push(simbolo);
-                    }
-                    break;
-                case '-':
-                    if(!puntos.isEmpty())
-                            puntos.pop();                      
-                    operadores.push(simbolo);
-                    signos.push(simbolo);                 
-                    break;
-                case '*':
-                    if(operadores.isEmpty() || operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
-                        res = false;
-                    else {
-                        if(!puntos.isEmpty())
-                            puntos.pop();                       
-                        operadores.push(simbolo); 
-                        if(!division.isEmpty()) {
-                            cero = contarPila(casoCero);
-                            total = 0;
-                            while(!casoCero.isEmpty()) 
-                                if(casoCero.pop().equals('0'))
-                                    total++;
-                            if(cero == total)
-                                res = false;
-                            division.pop();
-                        }  
-                        signos.push(simbolo);
-                    }                  
-                    break;
-                case '/':
-                    if(operadores.isEmpty() || operadores.peek().equals('+') || operadores.peek().equals('-') || operadores.peek().equals('/') || operadores.peek().equals('*'))
-                        res = false;
-                    else {
-                        if(!puntos.isEmpty())
-                            puntos.pop();
-                        operadores.push(simbolo);
-                        if(!division.isEmpty()) {
-                            cero = contarPila(casoCero);
-                            total = 0;
-                            while(!casoCero.isEmpty()) 
-                                if(casoCero.pop().equals('0'))
-                                    total++;
-                            if(cero == total)
-                                res = false;
-                            division.pop();
-                        }     
-                        signos.push(simbolo);                        
-                        division.push(simbolo);
-                    }                
-                    break;
-                case '.':
-                    if(puntos.isEmpty())
-                        puntos.push(simbolo);
-                    else
-                        res = false;
-                    operadores.push(simbolo);
-                    break;
-                default: 
-                    operadores.push(simbolo);
-                    if(!signos.isEmpty())
-                        if(signos.peek().equals('('))
-                            parentesis2.push(simbolo);
-                    if(!division.isEmpty())
-                        casoCero.push(simbolo);
+                            if(signos.peek().equals('('))
+                                parentesis2.push(simbolo);
+                        if(!division.isEmpty())
+                            casoCero.push(simbolo);
+                }
             }
             i++;
         }
@@ -208,7 +210,7 @@ public class Calculadora {
                 res = false;
         }
         return res;
-    }
+    }  
 
     public PilaADT<String> expresionCalculadora() {
         PilaArre cad = new PilaArre();
