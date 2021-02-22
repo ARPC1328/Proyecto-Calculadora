2033b89b8b843fc0fbc657370a29cedfa16ce5bc
fguillen-tejera <65374485+fguillen-tejera@users.noreply.github.com>
2021-02-21 20:36:41 -0600

    Update PilaADT.java

--- PilaADT.java
+++ PilaADT.java
@@ -6,12 +6,26 @@
 package calculadora;
 
 /**
- *
- * @author Brayansito
+ * La interface que contiene los métodos y funciones que debe tener una PilaADT.
+ * @author Anthoan, Andrés, Alejandro, Brayan, Fernando.
+ * @version 1.0
+ * @see PilaArre.java
  */
 public interface PilaADT <T>{
+ /**
+ * Método para agregar un elemento a la pila.
+ */
     public void push (T dato);
+/**
+ * Método para eliminar el último elemento agregado la pila.
+ */
     public T pop();
+/**
+ * Método para conocer si la pila está vacia.
+ */
     public boolean isEmpty();
+ /**
+ * Método para conocer el último elemento agregado a la pila.
+ */
     public T peek();
 }
